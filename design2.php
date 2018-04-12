6094f166722409a7d5b6193d4dc2811d18dc5e75
activeahmad <38321941+activeahmad@users.noreply.github.com>
2018-04-12 17:51:35 +0100

    @activeahmad
    stage 2 project

---
+++ design2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="figm.css">
+	<title></title>
+</head>
+<body>
+<div class="jumbotron">
+	<p>
+		WELCOME HOME
+	</p>
+	<h4>
+		LIFE IS TOO SIMPLE TO BE COMPLICATED BY ANYONE!
+	</h4>
+	<h5>
+		<?php
+
+// echo(date("F d, Y h:i:s", $timestamp));
+echo(date("Y-m-d h:i:sA"));
+?>
+	</h5>
+</div>
+</body>
+</html>
